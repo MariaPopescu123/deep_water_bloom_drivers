d3c3b631f6ea572b229d934f1ed07c2933dbc115
MariaPopescu <mariapopescu100@gmail.com>
2026-07-03 13:32:47 -0400

    added a comment to clarify warning message

--- 02_Datawrangling/00_run_all.R
+++ 02_Datawrangling/00_run_all.R
@@ -1,7 +1,13 @@
 # Run all data wrangling scripts in order
+# See README for information on what each script produces
 
 source("02_Datawrangling/01_water_level.R")
 source("02_Datawrangling/02_Phytos_dataframe.R")
+# After running 02_Phytos_dataframe.R, the following is just an aesthetic warning and can be ignored:
+# `geom_path()`: Each group consists of only one observation.
+# ℹ Do you need to adjust the group aesthetic?
+
+
 source("02_Datawrangling/03_Phytos_heatmaps_profiles.R")
 source("02_Datawrangling/04_photic_temp_thermo.R")
 source("02_Datawrangling/05_buoyancy_freq.R")
